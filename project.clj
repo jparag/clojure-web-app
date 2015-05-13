490cf53e03dbce2ba722338c9393d554709ef5d1
Parag Jain <ppjain@comscore.com>
2015-05-12 22:52:04 -0400

    Created Sample Contents from the referenced document

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject web-app "0.1.0"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.3.1"]
+                 [ring/ring-defaults "0.1.2"]
+		 [hiccup "1.0.2"]
+		 [org.clojure/java.jdbc "0.2.3"]
+		 [com.h2database/h2 "1.3.170"]
+		 [ring/ring-jetty-adapter "1.4.0-beta2"]]
+  :main web-app.handler
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler web-app.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
